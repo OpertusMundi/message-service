b7b82e3004f7e85bcb8a7f7226c7672d4f19ba1e
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2021-04-19 00:29:35 +0300

    Review docker-entrypoint.sh (errexit)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 set -u -e -o pipefail
-set -x
+
+[[ "${DEBUG:-false}" != "false" || "${XTRACE:-false}" != "false" ]] && set -x
 
 function _gen_configuration()
 {
+    if echo ${DATABASE_URL} | grep -v -q -e '^jdbc:postgresql:[/][/]' ; then
+        echo "DATABASE_URL does not seem like a JDBC PostgreSQL connection string" 1>&2 && exit 1 
+    fi
+    database_url=${DATABASE_URL}
+    database_username=${DATABASE_USERNAME}
+    database_password=$(cat ${DATABASE_PASSWORD_FILE} | tr -d '\n')
+
+    jwt_secret=$(cat ${JWT_SECRET_FILE} | tr -d '\n')
+    
     cat <<-EOD
-	spring.datasource.url = ${DATABASE_URL}
-	spring.datasource.username = ${DATABASE_USERNAME}
-	spring.datasource.password = $(cat ${DATABASE_PASSWORD_FILE} | tr -d '\n')
+	spring.datasource.url = ${database_url}
+	spring.datasource.username = ${database_username}
+	spring.datasource.password = ${database_password}
 	
-	opertus-mundi.security.jwt.secret = $(cat ${JWT_SECRET_FILE} | tr -d '\n')
+	opertus-mundi.security.jwt.secret = ${jwt_secret}
 	EOD
 }
-
-# Check environment and generate configuration
-
-if echo ${DATABASE_URL} | grep -v -q -e '^jdbc:postgresql:[/][/]' ; then
-   echo "The DATABASE_URL does not seem like a JDBC connection string" 1>&2 && exit 1
-fi
 
 runtime_profile=$(hostname | md5sum | head -c 32)
 _gen_configuration > ./config/application-${runtime_profile}.properties
